d61c1afefddf74fa663aa3f631411cb796cb6802
Revathi <rureva91@gmail.com>
2015-04-08 18:31:30 -0400

    update for put,post ,delete

--- src/main/java/credentials/testfile.java
+++ src/main/java/credentials/testfile.java
@@ -5,16 +5,24 @@
  */
 package credentials;
 
+import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
+import javax.json.stream.JsonParser;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -43,10 +51,10 @@ public class testfile {
     }
 
   
-     private String getResults(String query, String... params) {
+    private String getResults(String query, String... params) {
         JsonArrayBuilder productArray = Json.createArrayBuilder();
-        String numChanges = new String();
-        try (Connection conn = credentials.getConnection()) {
+        String myString = new String();
+         try (Connection conn = credentials.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement(query);
             for (int i = 1; i <= params.length; i++) {
                 pstmt.setString(i, params[i - 1]);
@@ -56,7 +64,7 @@ public class testfile {
 
             while (rs.next()) {
 
-                JsonObjectBuilder jsonobj = Json.createObjectBuilder()
+                JsonObjectBuilder jsonob = Json.createObjectBuilder()
                         .add("Name", rs.getString("Name"))
                         .add("Phone_number", rs.getInt("Phone_number"))
                         .add("Email_id", rs.getString("Email_id"))
@@ -66,16 +74,111 @@ public class testfile {
                         .add("Check_in", rs.getString("Check_in"))
                         .add("Check_Out", rs.getString("Check_Out"));
 
-                numChanges = jsonobj.build().toString();
-                productArray.add(jsonobj);
+                myString = jsonob.build().toString();
+                productArray.add(jsonob);
             }
 
         } catch (SQLException ex) {
             Logger.getLogger(testfile.class.getName()).log(Level.SEVERE, null, ex);
         }
         if (params.length == 0) {
-            numChanges = productArray.build().toString();
+            myString = productArray.build().toString();
+        }
+        return myString;
+    }
+    @POST
+    @Consumes("application/json")
+    public void doPost(String str) {
+        JsonParser parser = Json.createParser(new StringReader(str));
+        Map<String, String> map = new HashMap<>();
+        String name = "", value;
+
+        while (parser.hasNext()) {
+            JsonParser.Event evt = parser.next();
+            switch (evt) {
+                case KEY_NAME:
+                    name = parser.getString();
+                    break;
+                case VALUE_STRING:
+
+                    value = parser.getString();
+                    map.put(name, value);
+                    break;
+                case VALUE_NUMBER:
+                    value = Integer.toString(parser.getInt());
+                    map.put(name, value);
+                    break;
+            }
+        }
+        System.out.println(map);
+        String Name = map.get("Name");
+        String Phone_number = map.get("Phone_number");
+        String Email_id = map.get("Email_id");
+        String Room_type = map.get("Room_type");
+        String Card_Type = map.get("Card_Type");
+        String Card_Number = map.get("Card_Number");
+        String Check_in = map.get("Check_in");
+        String Check_Out = map.get("Check_Out");
+        doUpdate("insert into HotelReservation ( Name, Phone_number, Email_id, Room_type, Card_Type, Card_Number, Check_in, Check_Out) values ( ?, ?, ?, ?, ?, ?, ?, ?)", Name, Phone_number, Email_id, Room_type, Card_Type, Card_Number, Check_in, Check_Out);
+    }
+
+        private int doUpdate(String query, String... params) {
+        int numChanges = 0;
+        try (Connection conn = credentials.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(testfile.class.getName()).log(Level.SEVERE, null, ex);
         }
         return numChanges;
     }
+        
+ @PUT
+    @Path("{id}")
+    @Consumes("application/json")
+    public void doPut(@PathParam("id") String id, String str) {
+        JsonParser parser = Json.createParser(new StringReader(str));
+        Map<String, String> map = new HashMap<>();
+        String name = "", value;
+        while (parser.hasNext()) {
+            JsonParser.Event event = parser.next();
+            switch (event) {
+                case KEY_NAME:
+                    name = parser.getString();
+                    break;
+                case VALUE_STRING:
+                    value = parser.getString();
+                    map.put(name, value);
+                    break;
+                case VALUE_NUMBER:
+                    value = Integer.toString(parser.getInt());
+                    map.put(name, value);
+                    break;
+            }
+        }
+        System.out.println(map);
+
+        String Name = map.get("Name");
+        String Phone_number = map.get("Phone_number");
+        String Email_id = map.get("Email_id");
+        String Room_type = map.get("Room_type");
+        String Card_Type = map.get("Card_Type");
+        String Card_Number = map.get("Card_Number");
+        String Check_in = map.get("Check_in");
+        String Check_Out = map.get("Check_Out");
+        doUpdate("update HotelReservation set Name = ?, Phone_number = ?, Email_id = ?, Room_type = ?, Card_Type = ?, Card_Number = ?, Check_in = ?, Check_Out = ? where Email_id = ?", Name, Phone_number, Email_id, Room_type, Card_Type, Card_Number, Check_in,Check_Out,id);
+
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void doDelete(@PathParam("id") String id, String str) {
+        doUpdate("delete from HotelReservation where Email_id = ?", id);
+    }
+
 }
+
+
